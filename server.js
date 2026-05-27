734c1e0220516254468add119491529aa1922029
Ekansh Satsangi <ekanshsatsangi@gmail.com>
2026-05-28 01:36:22 +0530

    Configure Vercel deployment

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default app;
